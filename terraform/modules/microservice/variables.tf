ed26689646a18a1df96a8cf238d5fd0b9173473a
Abdulkhader081 <abdulkhadermd081@gmail.com>
2025-11-22 15:16:35 +0530

    Created variables.tf

---
+++ terraform/modules/microservice/variables.tf
@@ -0,0 +1,8 @@
+variable "name" {}
+variable "image_repository" {}
+variable "image_tag" {}
+variable "cluster_endpoint" {}
+variable "cluster_token" {}
+variable "client_certificate" {}
+variable "client_key" {}
+variable "cluster_ca_certificate" {}
