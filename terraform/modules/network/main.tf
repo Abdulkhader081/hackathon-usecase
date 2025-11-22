84d23c2d7814573815f2ee40e96a5a9094cef65f
Abdulkhader081 <abdulkhadermd081@gmail.com>
2025-11-22 13:43:16 +0530

    Update main.tf

--- terraform/modules/network/main.tf
+++ terraform/modules/network/main.tf
@@ -1,1 +1,13 @@
-resource 
+resource "google_compute_network" "vpc" {
+  name = "${var.project}-vpc"
+  auto_create_subnetworks = false 
+}
+resource "google_compute_subnetwork" "public" {
+  count = 2
+  name = "${var.project}-public-${count.index}"
+  ip_cidr_range = cidrsubnet(var.vpc_cidr, 4 , count.index)
+  region = var.region
+  network = google_compute_network.vpc.id 
+}
+resource "
+  
